b3f4ef8cfdbedccb5a8cc9c2a612c22ccaeeae33
Freilyn Bernabe <freilyn@bernabe.dev>
2025-03-11 13:33:23 -0400

    feat: Improve home page styling and structure

--- src/components/home/home-page.tsx
+++ src/components/home/home-page.tsx
@@ -1,7 +1,25 @@
 export default function HomePage() {
   return (
-    <div>
-      <h1>Generate PDF</h1>
-    </div>
+    <html>
+      <body
+        style={{
+          margin: 0,
+          fontFamily: "system-ui, -apple-system, BlinkMacSystemFont",
+          background: "#000",
+          color: "#fff",
+        }}
+      >
+        <div
+          style={{
+            height: "100vh",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <h1>Generate PDF</h1>
+        </div>
+      </body>
+    </html>
   );
 }
